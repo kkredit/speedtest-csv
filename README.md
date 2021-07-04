c30e6ed8a33382892bc45665ec84d6a02fcd5be4
Kevin Kredit <k.kredit.us@ieee.org>
2021-07-04 09:41:54 -0400

    Update script and create README.md

---
+++ README.md
@@ -0,0 +1,12 @@
+# Speedtest CSV
+
+Continually run internet speed tests and export results to a CSV file.
+
+Largely a wrapper script around [speedtest-cli](https://github.com/sivel/speedtest-cli).
+
+## Setup
+
+```bash
+poetry install
+poetry run pre-commit install
+```
--- speedtest_csv.py
+++ speedtest_csv.py
@@ -1,5 +1,8 @@
-import speedtest
+import pprint
 import time
+from datetime import datetime
+
+import speedtest
 
 
 def epoch_ts() -> int:
@@ -8,33 +11,65 @@ def epoch_ts() -> int:
 
 class SpeedTestRunner:
     def __init__(
-        self, period=60, samples=0, outfile="speedtest_results.csv", threads=1
+        self,
+        period=60,
+        samples=0,
+        outfile="speedtest_results.csv",
+        threads=1,
+        servers=None,
     ):
+        print("Initializing...")
         self._period = period
         self._samples = samples
         self._outfile = outfile
         self._threads = threads
         self._tester = speedtest.Speedtest()
-        self._tester.get_servers()
+        self._tester.get_servers(servers)
         self._tester.get_best_server()
+        self._initialize_csv()
+        print("Initialization complete")
 
     def run_continually(self) -> None:
         run = 0
         while self._samples <= 0 or run < self._samples:
             run += 1
-            print(f"Performing run {run}...")
+            print(f"\nPerforming run {run}...")
             duration = self.run_once()
             time.sleep(self._period - duration)
 
     def run_once(self) -> int:
         start_ts = epoch_ts()
+        self._reset_result_timestamp()
         self._tester.download(threads=self._threads)
         self._tester.upload(threads=self._threads)
-        # self._tester.results.share()
-        print(self._tester.results)
+        self._print_results()
+        self._append_csv()
         return epoch_ts() - start_ts
+
+    def _initialize_csv(self) -> None:
+        try:
+            with open(self._outfile, "x") as outfile:
+                outfile.write(speedtest.SpeedtestResults.csv_header() + "\n")
+        except FileExistsError:
+            pass
+
+    def _append_csv(self) -> None:
+        with open(self._outfile, "a") as outfile:
+            outfile.write(self._tester.results.csv() + "\n")
+
+    def _reset_result_timestamp(self) -> None:
+        self._tester.results.timestamp = datetime.now().isoformat()
+
+    def _print_results(self) -> None:
+        print("Results: ")
+        results = self._tester.results.dict()
+        filtered = {
+            k: v for k, v in results.items() if k not in ["client", "server", "share"]
+        }
+        filtered.update({"isp": results.get("client", {}).get("isp")})
+        pprint.pprint(filtered)
 
 
 if __name__ == "__main__":
     print("Now collecting speedtest data!")
-    SpeedTestRunner().run_continually()
+    SpeedTestRunner(servers=[34750]).run_continually()
